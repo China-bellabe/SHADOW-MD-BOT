d9bf3d70118b686a4b1e137d84069592648ad966
China-bellabe <chinabelabe@gmail.com>
2024-11-23 18:17:43 -0500

    Update cmd.js

--- commandes/cmd.js
+++ commandes/cmd.js
@@ -1,4 +1,3 @@
-
 const util = require('util');
 const fs = require('fs-extra');
 const { zokou } = require(__dirname + "/../framework/zokou");
@@ -6,161 +5,76 @@ const { format, styletext } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../config");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
 
-adams({ nomCom: "cmd", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+// Command to display bot information and commands
+zokou({ nomCom: "cmd", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
     let { cm } = require(__dirname + "/../framework/zokou");
     var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
-    }
+    var mode = (s.MODE.toLocaleLowerCase() !== "yes") ? "private" : "public";
 
-
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
+    // Organize commands by category
+    cm.map(async (com) => {
+        if (!coms[com.categorie]) {
             coms[com.categorie] = [];
+        }
         coms[com.categorie].push(com.nomCom);
     });
 
     moment.tz.setDefault("Africa/Nairobi");
 
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
+    // Create a date and time in GMT
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
 
-  let infoMsg =  `
-  *Hey🖐️* *${nomAuteurMessage}*
-  
-  *SHADOW MD IS RUNNING WITH [${cm.length}0] COMMANDS*
+    // Information message
+    let infoMsg = `
+Hey🖐️ ${nomAuteurMessage}
 
-  *More commands will be out soon*
- 
-  🌟🌟🌟
- `;
-    
-let menuMsg = `
-> ⏲️ ᴛɪᴍᴇ: ${temps}
-> 📅 ᴅᴀᴛᴇ: ${date} 
+SHADOW MD IS RUNNING WITH [${cm.length}] COMMANDS
 
+More commands will be out soon
 
-> Made by : ©SHADOW-WRLD 
+🌟🌟🌟
 `;
 
-   var lien = mybotpic();
+    // Menu message
+    let menuMsg = `
+⏲️ ᴛɪᴍᴇ: ${temps}
+📅 ᴅᴀᴛᴇ: ${date}
 
-   if (lien.match(/\.(mp4|gif)$/i)) {
-    try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *shadow*, déveloper Shadow Tech" , gifPlayback : true }, { quoted: ms });
+Made by : ©SHADOW-WRLD
+`;
+
+    var lien = mybotpic();
+
+    // Check the type of the link and send appropriate message
+    if (lien.match(/.(mp4|gif)$/i)) {
+        try {
+            await zk.sendMessage(dest, {
+                video: { url: lien },
+                caption: infoMsg + menuMsg,
+                footer: "Je suis shadow, développeur Shadow Tech",
+                gifPlayback: true
+            }, { quoted: ms });
+        } catch (e) {
+            console.error("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+        }
+    } else if (lien.match(/.(jpeg|png|jpg)$/i)) {
+        try {
+            await zk.sendMessage(dest, {
+                image: { url: lien },
+                caption: infoMsg + menuMsg,
+                footer: "Je suis shadow, développeur Shadow Tech"
+            }, { quoted: ms });
+        } catch (e) {
+            console.error("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+        }
+    } else {
+        repondre(infoMsg + menuMsg);
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *shadow*, déveloper Shadow Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
-}); 
-
-
-/*const util = require('util');
-const fs = require('fs-extra');
-const { zokou } = require(__dirname + "/../framework/zokou");
-const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
-const moment = require("moment-timezone");
-const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
-
-zokou({ nomCom: "script", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
-    }
-
-
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
-    moment.tz.setDefault('Etc/GMT');
-
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-  let infoMsg =  `
-   *BMW MD IMPORTANT INFO* 
-❒───────────────────❒
-*GITHUB LINK*
-> https://github.com/ShadowWrld/SHADOW-MD-BOT
-
-*FOR MORE INFO TAP ON THE LINK BELOW*
-> https://github.com/ShadowWrld/⁠
-╭───────────────────❒
-│❒⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-│❒⁠⁠⁠⁠ *DEV* : *SHADOW-WRLD*
-⁠⁠⁠⁠╰───────────────────❒
-  `;
-    
-let menuMsg = `
-     SHADOW-WRLD SCIENCE
-
-❒────────────────────❒`;
-
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
-    try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
-});*/
+});
